69dfbe1f15617550f153781103fdf3b082f3d33b
nonebula <147166151+nonebula@users.noreply.github.com>
2025-10-02 13:31:51 +0100

    JP - arrays

--- src/lesson34VarArgs/VarArgs.java
+++ src/lesson34VarArgs/VarArgs.java
@@ -3,5 +3,68 @@ package lesson34VarArgs;
 public class VarArgs {
     public static void main(String[] args) {
 
+        //varargs - variable arguments
+            // allow a method to accept a varying # of arguments
+            // makes methods more flexible, no need for overloaded methods
+            // java will pack the arguments into an array
+            // ... (ellipsis)
+
+        System.out.println(add(1,2,3,4));
+
+        System.out.println(average(1,2,3,4));
+
     }
+
+
+//i.e. something like this is redundant, and can be done as above instead:
+    //    static double add(int a, int b){
+    //        return a+b;
+    //    };
+    //    static double add(int a, int b, int c){
+    //        return a+b+c;
+    //    }
+    //    static double add(int a, int b, int c, int d){
+    //        return a+b+c+d;
+    //    }
+    //    static  double add(int a, int b, int c, int d, int e){
+    //        return a+b+c+d+e;
+    //    }
+    //    static double add(int a, int b, int c, int d, int e, int f){
+    //        return a+b+c+d+e+f;
+    //    }
+    //    static double add(int a, int b, int c, int d, int e, int f, int g){
+    //        return a+b+c+d+e+f+g;
+    //    }
+
+    //this is farrr more efficient
+    static int add(int... numbers) {
+        int sum = 0;
+
+        //this avoids any NaN issues
+        if(numbers.length == 0) {
+            return 0;
+        }
+
+        for(int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    static double average(double... numbers) {
+        double sum = 0;
+
+        //this avoids any NaN issues
+        if(numbers.length == 0) {
+            return 0;
+        }
+
+        for(double number : numbers) {
+            sum += number;
+        }
+
+        return sum/numbers.length;
+    }
+
 }
--- src/lesson35TwoDArrays/TwoDArrays.java
+++ src/lesson35TwoDArrays/TwoDArrays.java
@@ -1,7 +1,55 @@
 package lesson35TwoDArrays;
 
+import java.util.Arrays;
+
 public class TwoDArrays {
     public static void main(String[] args) {
+
+        //2D array = also known as multidimensional arrays
+        //An array where each element is an array
+        //Useful for storing a matrix of data
+
+        String[] fruits = {"apple", "orange", "banana"};
+        String[] vegetables = {"potato", "onion", "carrot"};
+        String[] meats = {"chicken", "pork", "beef", "fish"};
+
+        String[][] groceries = {fruits, vegetables, meats};
+
+        for(String[] foods: groceries) {
+            for(String food: foods) {
+                System.out.print(food + " ");
+            }
+            System.out.println();
+        }
+
+        String[][] beverages = {{"water", "soda", "squash"},
+                {"red wine", "white wine", "rose"},
+                {"beer", "IPA", "cider"}};
+
+        for(String[] drinks: beverages) {
+            for(String beverage: drinks) {
+                System.out.print(beverage + " ");
+            }
+            System.out.println();
+        }
+
+        //first index = row, second index = column
+        groceries[0][0] = "pineapple";
+        groceries[0][1] = "grapefruit";
+        groceries[1][0] = "potato";
+
+        char[][] telephone = {{'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'},
+                {'*', '0', '#'},
+        };
+
+        for(char[] row : telephone) {
+            for(char number: row) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
